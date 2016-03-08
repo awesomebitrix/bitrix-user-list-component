97de41f15849bebf5161e17a67bf6fcaa1e77af4
gebvlad <sij.ambient.music@gmail.com>
2016-03-08 14:57:06 +0300

    Create .description.php

---
+++ bitrix/components/my_test_components/user.list/.description.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Файл описания компонента
+ */
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+use \Bitrix\Main\Localization\Loc as Loc;
+Loc::loadMessages(__FILE__);
+
+$arComponentDescription = [
+    'NAME'        => Loc::getMessage('USER_LIST_NAME'),
+    'DESCRIPTION' => Loc::getMessage('USER_LIST_DESCRIPTION'),
+   // 'ICON'        => '/images/news_list.gif',
+    'SORT'        => 20,
+    'CACHE_PATH'  => 'Y',
+    'PATH'        => [
+        'ID' => 'content',
+    ],
+];
