ccf9b985b31c579ce6e0c8838ea2f3f0e0699b7c
gebvlad <sij.ambient.music@gmail.com>
2016-03-08 14:58:55 +0300

    Create .parameters.php

---
+++ bitrix/components/my_test_components/user.list/lang/ru/.parameters.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Языковой файл для описания параметров компонента
+ */
+$MESS['USER_LIST_PARAMS_PAGER_SETTINGS'] = 'Настройки постраничной навигации';
+$MESS['USER_LIST_PARAMS_SHOW_ONLY_ACTIVE_USERS'] = 'Показывать только активных пользователей';
+$MESS['USER_LIST_PARAMS_SORT_BY'] = 'Сортировать по полю';
+$MESS['USER_LIST_PARAMS_SORT_ORDER'] = 'Порядок сортировки';
+$MESS['USER_LIST_PARAMS_FIELDS'] = 'Пользовательские поля';
+$MESS['USER_LIST_PARAMS_USER_FIELDS'] = 'Кастомные пользовательские поля';
+$MESS['USER_LIST_PARAMS_SHOW_ALL'] = 'Показывать весь список пользователей';
+$MESS['USER_LIST_PARAMS_SHOW_NAV'] = 'Показывать постраничную навигацию';
+$MESS['USER_LIST_PARAMS_PER_PAGE'] = 'Элементов на странице';
+$MESS['USER_LIST_PARAMS_NAV_TITLE'] = 'Загаловок постраничной навигации';
+$MESS['USER_LIST_PARAMS_NAV_TEMPLATE'] = 'Шаблон постраничной навигации';
+$MESS['USER_LIST_PARAMS_AJAX_MODE'] = 'Режим AJAX';
+$MESS['USER_LIST_PARAMS_CACHE_TIME'] = 'Время кеширования';
+$MESS['USER_LIST_PARAMS_CACHE_TYPE'] = 'Кеширование';
